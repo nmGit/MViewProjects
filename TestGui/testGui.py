8c650c1b09405c2425c181fd09911725fae83c23
Noah Meltzer <noah.a.meltzer@gmail.com>
2018-10-12 19:34:44 -0500

    add more tests

--- TestGui/testGui.py
+++ TestGui/testGui.py
@@ -11,7 +11,7 @@ from MDevices.MVirtualDevice import MVirtualDevice
 from MDevices.Mhdf5Device import Mhdf5Device
 
 
-import grapher as alexGrapher
+#import grapher as alexGrapher
 from MNodeEditor.MNodes import runningAverage
 from MNodeEditor import MNodeTree
 from CustomMViewTiles.tetris import tetris
@@ -28,18 +28,29 @@ class nViewer:
         self.gui = MGui.MGui()
 
         self.dd1 = MDummyDevice("Dummy 1")
-        self.dd1.addPlot()
+
         self.dd1.addParameter("Field 1")
         self.dd1.addParameter("Field 2")
         self.dd1.addParameter("Field 3")
+        self.dd1.addParameter("Field 4")
+        self.dd1.addParameter("Field 5")
+        self.dd1.addParameter("Field 6")
+        self.dd1.addParameter("Field 7")
+        self.dd1.addParameter("Field 8")
+        self.dd1.addParameter("Field 9")
         self.dd1.log(True)
+        self.dd1.addPlot()
         self.dd1.begin()
         self.gui.addDevice(self.dd1)
 
         self.dd2 = MDummyDevice("Dummy 2")
         self.dd2.addParameter("Field 1")
-        self.dd2.addPlot()
+        self.dd2.addParameter("Field 2")
+        self.dd2.addParameter("Field 3")
+        self.dd2.addParameter("Field 4")
+        self.dd2.addParameter("Field 5")
         self.dd2.log(True)
+        self.dd2.addPlot()
         self.dd2.begin()
         self.gui.addDevice(self.dd2)
 
